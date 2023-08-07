3b524dd8c124744ef28fffea5a07c31fcef65f3e
Erdemg <erdemgulhan@gmail.com>
2023-08-07 15:15:38 +0300

    new file added

---
+++ copilot1.js
@@ -0,0 +1,31 @@
+function calculatedaysbetweendates(date1, date2) {
+    var diff = Math.floor(date2.getTime() - date1.getTime());
+    var day = 1000 * 60 * 60 * 24;
+    var days = Math.floor(diff / day);
+    return days;
+}           
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    function sendtweet() {
+        // code to send tweet   
+        var date1 = new Date("12/25/2016");
+        var date2 = new Date();
+        var days = calculatedaysbetweendates(date1, date2);
+        var tweet = "There are " + days + " days until Christmas!";
+        console.log(tweet);
+    
+    }
+    sendtweet(); // added function call
+}
+
+
+// find all images without alternate text
+// and give them a red border
+var images = document.getElementsByTagName("img");
+for (var i = 0; i < images.length; i++) {
+    var image = images[i];
+    if (image.alt == "") {
+        image.style.border = "5px solid red";
+    }
+}
